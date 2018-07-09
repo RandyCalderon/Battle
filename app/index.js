e167d23d98127e1f5584f097f4145e60c3a147e6
RandyCalderon <randycweb@gmail.com>
2018-07-08 17:46:13 -0700

    need to add match.params id

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+var ReactDOM = require('react-dom');
+import './index.css';
+var App = require('../components/App')
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('app')
+);
